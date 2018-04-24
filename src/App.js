04b052a22e42fd1e0aebe44ccd8a380615a448d1
wonktnodi <wonktnodiq@gmail.com>
2018-04-24 19:46:07 +0800

    add route, store and reducer

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Provider } from "react-redux";
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+import configureStore from "./store/configureStore";
+
+import Home from './containers/Home';
+
+const store = configureStore();
+
+const App = () => (
+  <Provider store={store}>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </BrowserRouter>
+  </Provider>
+);
 
 export default App;
---
+++ src/containers/Home/index.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+import Layout from "../../components/Layout";
+
+import logo from "../../logo.svg";
+import "../../App.css";
+
+export class Home extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Layout />
+      </div>
+    );
+  }
+}
+
+export default Home;
